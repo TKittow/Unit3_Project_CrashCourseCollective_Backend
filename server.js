706c01215e3fd705c25ed9389cf3a767d67466b0
kiwicj <cam.orr93@gmail.com>
2024-02-08 14:36:34 +0000

    Thursday lunch

--- server.js
+++ server.js
@@ -349,7 +349,6 @@ app.put('/project/:id', async (req, res) => {
     try {
         const projectId = req.params.id
         const updatedProjectData = req.body
-
         const existingProject = await Project.findById(projectId)
 
         if (!existingProject) {
